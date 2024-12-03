ddbb0160affbb4b0efea786d6ff17cbb4f28627c
KiranRajeshRL <kiranrajeshrl2003@gmail.com>
2024-12-03 16:44:55 +0530

    first push

---
+++ markor-master/markor-master/app/src/main/java/net/gsantner/markor/activity/openeditor/OpenFromShortcutOrWidgetActivity.java
@@ -0,0 +1,31 @@
+package net.gsantner.markor.activity.openeditor;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import net.gsantner.markor.activity.DocumentActivity;
+import net.gsantner.markor.activity.MarkorBaseActivity;
+
+/**
+ * This Activity exists solely to launch DocumentActivity with the correct intent
+ * it is necessary as widget and shortcut intents do not respect MultipleTask etc
+ */
+public class OpenFromShortcutOrWidgetActivity extends MarkorBaseActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        launchActivityAndFinish(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(final Intent intent) {
+        super.onNewIntent(intent);
+        launchActivityAndFinish(intent);
+    }
+
+    private void launchActivityAndFinish(Intent intent) {
+        DocumentActivity.launch(this, intent);
+        finish();
+    }
+}
